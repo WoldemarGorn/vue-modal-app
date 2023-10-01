e5344c7593cafcacfa1aad8515bb4cce44812ad8
Woldemar Gorn <wg84@gmx.de>
2023-10-01 10:06:51 +0200

    css handling, using props, emitting events and they modifier, slots and teleport feater of vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,32 @@
 <template>
   <h1>{{ title }}</h1>
-  <Modal/>
+  <p>Welcome ...</p>
+  <teleport to="#modals" v-if="showModal">
+    <!-- Slots -->
+    <Modal theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">login</a>
+        <a href="#">logout</a>
+      </template>
+      <h1>{{ modalHeader }}</h1>
+      <p>{{ modalText }}</p>
+    </Modal>
+  </teleport>
+
+  <teleport  to="#modals" v-if="showModalTwo">
+    <!-- Slots -->
+    <Modal theme="login" @close="toggleModalTwo">
+      <template v-slot:contetnt>
+        <div>
+          <p>Content two</p>
+        </div>
+      </template>
+      <h1>Header text two</h1>
+    </Modal>
+  </teleport>
+  <!-- click event modifier -->
+  <button @click.alt="toggleModal">open modal (alt)</button>
+  <button @click="toggleModalTwo">open modal two</button>
 </template>
 
 <script>
@@ -13,14 +39,29 @@ export default {
   },
   data() {
     return {
-      title: 'My first vue app!!!'
+      title: 'My first vue app!!!',
+      modalHeader: "Header text slot",
+      modalText: "Content text slot",
+      showModal: false,
+      modalHeaderTwo: "",
+      modalTextTwo: "Content text two",
+      showModalTwo: false
     }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo
+    },
+
   }
 }
 </script>
 
 <style>
-#app {
+#app, #modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,11 +1,30 @@
 <template>
-    <div class="backdrop">
-        <div class="modal">
-            <p>modal content</p>
+    <!-- click event modifier -->
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{ sale: theme === 'sale' }">
+            <slot>default content if not set by customer</slot>
+            <slot name="contetnt"></slot>
+            <div class="actions">
+                <slot name="links"></slot>
+            </div>
         </div>
     </div>
 </template>
 
+<script>
+//Using props to make template dynamic
+export default {
+    props: ['theme'],
+    methods: {
+        closeModal() {
+            //Emitting events
+            this.$emit('close')
+        }
+    }
+}
+</script>
+
+<!-- To scoped this css style to this template use <style scoped> or like belove .modal(name of componen)... -->
 <style>
 .modal {
     width: 400px;
@@ -21,5 +40,42 @@
     background: rgba(0, 0, 0, 0.5);
     width: 100%;
     height: 100%;
+}
+
+.modal h1 {
+    color: rgb(29, 139, 103);
+    size: 20px;
+    border-style: none;
+}
+
+.modal p {
+    font-style: normal;
+}
+
+.modal .actions {
+    text-align: center;
+    margin: 30px 0 10px 0;
+}
+
+.modal .actions a {
+    color: #333;
+    padding: 8px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    text-decoration: none;
+    margin: 10px;
+}
+
+.modal.sale {
+    background: crimson;
+    color: white;
+}
+
+.modal.sale h1 {
+    color: white;
+}
+
+.modal.sale .actions a {
+    color: white;
 }
 </style>
